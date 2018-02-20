7b2c944f0ee9a2b4464d614ab02cf490fe548e0d
Ecconia <github-commit@ecconia.com>
2018-02-20 15:55:17 +0100

    Added command-framework

--- src/main/java/com/ecconia/rsisland/plugin/region/RegionPlugin.java
+++ src/main/java/com/ecconia/rsisland/plugin/region/RegionPlugin.java
@@ -1,8 +1,25 @@
 package com.ecconia.rsisland.plugin.region;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.ecconia.rsisland.plugin.region.command.framework.CommandHandler;
+import com.ecconia.rsisland.plugin.region.command.framework.Subcommand;
 
 public class RegionPlugin extends JavaPlugin
 {
-
+	public static final String prefix = ChatColor.WHITE + "[" + ChatColor.GOLD + "Region" + ChatColor.WHITE + "] ";
+	
+	@Override
+	public void onEnable()
+	{
+		initCommand();
+	}
+	
+	private void initCommand()
+	{
+		getCommand("reg").setExecutor(new CommandHandler(this, new Subcommand[]{
+				
+		}));
+	}
 }
