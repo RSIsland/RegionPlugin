8b349fff191a0a7a585826e2cdb5de60a8530095
Ecconia <eccohirsch@gmail.com>
2018-06-18 14:25:24 +0200

    Getting SelectionAPI
    Will be used to edit and create new regions.

--- src/main/java/com/ecconia/rsisland/plugin/region/RegionPlugin.java
+++ src/main/java/com/ecconia/rsisland/plugin/region/RegionPlugin.java
@@ -1,19 +1,30 @@
 package com.ecconia.rsisland.plugin.region;
 
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.ecconia.rsisland.framework.cofami.CommandHandler;
 import com.ecconia.rsisland.framework.cofami.Feedback;
 import com.ecconia.rsisland.framework.cofami.GroupSubcommand;
+import com.ecconia.rsisland.plugin.selection.api.SelectionAPI;
 
 public class RegionPlugin extends JavaPlugin
 {
 	public static final String prefix = ChatColor.WHITE + "[" + ChatColor.GOLD + "Region" + ChatColor.WHITE + "] ";
+	private SelectionAPI selectAPI;
 	
 	@Override
 	public void onEnable()
 	{
+		{
+			RegisteredServiceProvider<SelectionAPI> provider = getServer().getServicesManager().getRegistration(SelectionAPI.class);
+			if(provider != null)
+			{
+				selectAPI = (SelectionAPI) provider.getProvider();
+			}
+		}
+		
 		initCommand();
 	}
 	
@@ -25,5 +36,13 @@ public class RegionPlugin extends JavaPlugin
 			,new CommandAppend(this)
 			,new CommandRemove(this)
 		));
+
+	public SelectionAPI getSelectAPI() throws NoSelectionPluginException
+	{
+		if(selectAPI == null)
+		{
+			throw new NoSelectionPluginException();
+		}
+		return selectAPI;
 	}
 }
